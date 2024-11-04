b32bf10f26d9ecf23d41ec178afa57c5d8ec69c7
simyrota <ryoma.saito2@gmail.com>
2024-11-04 17:24:00 +0900

    HandsOn 3章まで完了

---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  // 1行の最大文字数
+  printWidth: 100,
+  // 文字列はシングルクォートで囲む
+  singleQuote: true,
+}
---
+++ app/_components/NewsList/index.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import styles from './index.module.css';
+import type { News } from '@/app/_libs/microcms';
+import Category from '@/app/_components/Category/index';
+import Date from '@/app/_components/Date/index';
+
+type Props = {
+  news: News[];
+};
+
+export default function NewsList({ news }: Props) {
+  if (news.length === 0) {
+    return <p>記事がありません</p>;
+  }
+  return (
+    <ul>
+      {news.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <div className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/no-image.png"
+              alt="no-image"
+              width={1200}
+              height={630}
+            />
+            <dl className={styles.content}>
+              <dt className={styles.title}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
+              </dd>
+            </dl>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
