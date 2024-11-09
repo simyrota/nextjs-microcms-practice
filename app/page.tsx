8daa52607b850974c5a083ea9af718d87989a0a6
simyrota <ryoma.saito2@gmail.com>
2024-11-09 18:36:55 +0900

    HandsOn 5章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,44 @@
 import Image from 'next/image';
+import { getNewsList } from '@/app/_libs/microcms';
+import { TOP_NEW_LIMIT } from '@/app/_constants';
 import styles from './page.module.css';
-import ButtonLink from '@/app/_components/ButtonLink';
-import type { News } from '@/app/_libs/microcms';
 import NewsList from './_components/NewsList';
+import ButtonLink from '@/app/_components/ButtonLink';
 
-const data: { contents: News[] } = {
-  contents: [
-    {
-      id: '1',
-      title: '渋谷にオフィスを移転しました',
-      category: {
-        name: '更新情報',
-      },
-      publishedAt: '2024/11/03',
-      createdAt: '2024/11/03',
-    },
-    {
-      id: '2',
-      title: '当社CEOが業界リーダーTOP30に選出されました',
-      category: {
-        name: '更新情報',
-      },
-      publishedAt: '2024/11/02',
-      createdAt: '2024/11/02',
-    },
-    {
-      id: '3',
-      title: 'テストの記事です',
-      category: {
-        name: '更新情報',
-      },
-      publishedAt: '2024/11/01',
-      createdAt: '2024/11/01',
-    },
-  ],
-};
+// const data: { contents: News[] } = {
+//   contents: [
+//     {
+//       id: '1',
+//       title: '渋谷にオフィスを移転しました',
+//       category: {
+//         name: '更新情報',
+//       },
+//       publishedAt: '2024/11/03',
+//       createdAt: '2024/11/03',
+//     },
+//     {
+//       id: '2',
+//       title: '当社CEOが業界リーダーTOP30に選出されました',
+//       category: {
+//         name: '更新情報',
+//       },
+//       publishedAt: '2024/11/02',
+//       createdAt: '2024/11/02',
+//     },
+//     {
+//       id: '3',
+//       title: 'テストの記事です',
+//       category: {
+//         name: '更新情報',
+//       },
+//       publishedAt: '2024/11/01',
+//       createdAt: '2024/11/01',
+//     },
+//   ],
+// };
 
-export default function Home() {
-  const sliceData = data.contents.slice(0, 2);
+export default async function Home() {
+  const data = await getNewsList({ limit: TOP_NEW_LIMIT });
   return (
     <>
       <section className={styles.top}>
@@ -57,7 +58,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsList news={sliceData} />
+        <NewsList news={data.contents} />
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
         </div>
