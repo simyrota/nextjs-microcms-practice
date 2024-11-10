4cf246cc78b4c38a4dfd6f1bc94b751e1463fe40
simyrota <ryoma.saito2@gmail.com>
2024-11-10 23:21:45 +0900

    HandsOn 10章まで完了 ※Google Analyticsは除く

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,30 @@
-import "./globals.css";
-import Header from "./_components/Header";
-import Footer from "./_components/Footer";
+import './globals.css';
+import type { Metadata } from 'next';
+import Header from './_components/Header';
+import Footer from './_components/Footer';
+
+/**
+ * Todo: 以下のmetadataを実際のサイトに合わせて変更する
+ * HansOnの都合上、localhost:3000に設定しています
+ */
+export const metadata: Metadata = {
+  metadataBase: new URL('https://localhost:3000'),
+  title: {
+    template: '%s | シンプルなコーポレートサイトサンプル',
+    default: 'シンプルなコーポレートサイトサンプル',
+  },
+  description:
+    '「Next.js＋ヘッドレスCMSではじめる！ かんたん・モダンWebサイト制作入門」で作成されるサイトです。',
+  openGraph: {
+    title: 'シンプルなコーポレートサイトサンプル',
+    description:
+      '「Next.js＋ヘッドレスCMSではじめる！ かんたん・モダンWebサイト制作入門」で作成されるサイトです。',
+    images: ['/ogp.png'],
+  },
+  alternates: {
+    canonical: 'https://localhost:3000',
+  },
+};
 
 export default function RootLayout({
   children,
